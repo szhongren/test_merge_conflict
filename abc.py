ae52ee38dd7bd2fca1e0f4b9c60bca151f3924e3
Zhongren Shao <shao@bigeye.com>
2023-11-18 10:20:32 -0600

    third commit

--- abc.py
+++ abc.py
@@ -3,3 +3,5 @@ import requests
 res = requests.get("http://some-url.com")
 
 res2 = requests.post("http://some-other-url.com", body={})
+
+res3 = requests.post("http://yet-some-other-url.com", body={})
